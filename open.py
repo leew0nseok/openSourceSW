271659ec2879d210dcdbfe4aee1dc9c418e0addf
1eewonseok <90885230+1eewonseok@users.noreply.github.com>
2022-03-18 19:23:38 +0900

    Update open.py

--- open.py
+++ open.py
@@ -3,10 +3,13 @@
 print("본인의 이름, 학번, 전공, 취미 순으로 입력하세요(뛰어쓰기로 구분합니다)")
 name, SchoolNum, Major, habbit = map(str, input().split())
 
+age = '23'
+
 # name = '이원석'
 # SchoolNum = '201902702'
 # Major = '컴퓨터전자시스템공학부'
 # habbit = '운동'
+
 
 print("Hello World!\n")
 print(f"이름 = {name}, 학번 = {SchoolNum}, 전공 = {Major}, 취미 = {habbit}")
